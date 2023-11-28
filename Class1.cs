ba78afdd706b00115eefa1e9394d9ccdff4c0b26
Мария Бузникова <buznikova2003@gmail.com>
2023-11-28 11:16:06 +0300

    Добавьте файлы проекта.

---
+++ Class1.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ОдномерныйМассив
+{
+    class Class1
+    {
+        public class Massiv
+        {
+            public int size = 0;
+            public int[] mass;
+
+            public Massiv(int s)
+            {
+                size = s;
+                mass = new int[size];
+            }
+
+            public void InputData(params int[] m) //params
+            {
+                if (m.Length == 0)
+                {
+                    Console.WriteLine("Массив пуст!");
+                }
+                else
+                {
+                    for (int i = 0; i < m.Length; i++)
+                    {
+                        mass[i] = m[i];
+                    }
+                }
+            }
+            public void InputDataRandom()
+            {
+                int[] mass1 = new int[size];
+                Random r = new Random();
+                if (size == 0)
+                {
+                    Console.WriteLine("Массив не существует!!!");
+                }
+                else
+                {
+                    
+                    for (int i = 0; i < size; i++)
+                    {
+                        mass1[i] = r.Next(0, 101);
+                    }
+                }
+                mass = mass1;
+            }
+            public void Print()
+            {
+                Console.Write("Первый индекс: ");
+                int s = int.Parse(Console.ReadLine());
+                Console.Write("Конечный индекс: ");
+                int f = int.Parse(Console.ReadLine());
+                if(s>f)
+                {
+                    Console.WriteLine("Ошибка ввода!");
+                }
+                if (size == 0)
+                {
+                    Console.WriteLine("Массив пуст!!!");
+                }
+                if (f > size)
+                {
+                    Console.WriteLine("Конечный индекс вышел за пределы массива!!!");
+                }
+                else
+                {
+                    for (int i = s; i < f; i++)
+                    {
+                        Console.Write(mass[i] + "; ");
+                    }
+                    Console.WriteLine();
+                }
+            }        
+            public int[] FindValue(int el)
+            {
+                int new_s = 0;
+                //определяем размер нового массива индексов
+                for (int i = 0; i < mass.Length; i++)
+                {
+                    if (mass[i] == el)
+                    {
+                        new_s++;
+                    }
+                }
+                int[] FindValue = new int[new_s];
+                int p = 0;
+                for (int i = 0; i < mass.Length; i++)
+                {
+                    if (mass[i] == el)
+                    {
+                        FindValue[p] = i;
+                        p++;
+                    }
+                }
+                return FindValue;
+            }
+            public int[] DelValue(int value)
+            {
+                int count = 0;
+
+                // кол-во элементов, которое надо удалить
+                for (int i = 0; i < mass.Length; i++)
+                {
+                    if (mass[i] == value)
+                    {
+                        count++;
+                    }
+                }
+                int[] NewMass = new int[mass.Length - count];
+                int index = 0;
+
+                // копирование элементов из исходного массива в новый массив
+                for (int i = 0; i < mass.Length; i++)
+                {
+                    if (mass[i] != value)
+                    {
+                        NewMass[index] = mass[i];
+                        index++;
+                    }
+                }
+                size -= count;
+                mass = NewMass;
+                return NewMass;
+            }
+            public int FindMax()
+            {
+                int max = 0;
+                for (int i = 1; i < mass.Length; i++)
+                {
+                    if (mass[i] > max)
+                    {
+                        max = mass[i];
+                    }
+                }
+                Console.WriteLine("Максимальное значение: " + max);
+                return max;
+            }
+            public Massiv Add(in Massiv mass2)
+            {
+                if (mass2.size != size)
+                {
+                    Console.WriteLine("Разные длины массивов");
+                    return new Massiv(0);
+                }
+                else
+                {
+                    Massiv rez = new Massiv(size);
+                    int[] vs = new int[size];
+                    for (int i = 0; i < size; i++)
+                    {
+                        vs[i] = mass[i] + mass2.mass[i];
+                    }
+                    rez.InputData(vs);
+                    return rez;
+                }
+
+
+            }
+            public void Sort()
+            {
+                int n = mass.Length;
+
+                // Алгоритм сортировки пузырьком (Bubble Sort)
+                for (int i = 0; i < n - 1; i++)
+                {
+                    for (int j = 0; j < n - i - 1; j++)
+                    {
+                        if (mass[j] > mass[j + 1])
+                        {
+                            // Обмен значениями текущего элемента и следующего элемента
+                            int buf = mass[j];
+                            mass[j] = mass[j + 1];
+                            mass[j + 1] = buf;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using static ОдномерныйМассив.Class1;
+
+namespace ОдномерныйМассив
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Massiv mass1 = new Massiv(11);
+            mass1.InputData(5, 2, 4, 2, 1, 7, 7, 15, 3, 2, 9);
+            Console.WriteLine("Massive1: ");
+            for (int i = 0; i < mass1.size; i++)
+            {
+                Console.Write(mass1.mass[i] + "; ");
+            }
+            Console.WriteLine();
+            mass1.Print();
+            Console.WriteLine("Поиск числа\nКакое число ищем?");
+            int a = int.Parse(Console.ReadLine());
+            int[] indMass = mass1.FindValue(a);
+            Console.Write($"\nИндексы {a} в массиве: ");
+            for (int i = 0; i < indMass.Length; i++)
+            {
+                Console.Write(indMass[i] + "; ");
+            }
+            Console.WriteLine();
+            mass1.Sort();
+            Console.WriteLine("Сортировка \nОтсортированный массив 1");
+            for (int i = 0; i < mass1.size; i++)
+            {
+                Console.Write(mass1.mass[i] + "; ");
+            }
+            Console.WriteLine();
+            Console.WriteLine("Удаление числа из массива \nКакое число удалить?");
+            int d = int.Parse(Console.ReadLine());
+            
+            int[] delMass = mass1.DelValue(d);
+            Console.Write($"\nУдаляем число {d}: ");
+            Console.WriteLine();
+            mass1.Print();
+            
+            Massiv mass2 = new Massiv(mass1.size);
+            Console.WriteLine("Massive2: ");
+            mass2.InputDataRandom();
+            for (int i = 0; i < mass2.size; i++)
+            {
+                
+                Console.Write(mass2.mass[i] + "; ");
+            }
+
+            Console.Write("\nСкладываем первый и второй массив: ");
+            Massiv rez1 = mass1.Add(mass2);
+            for (int i = 0; i < rez1.size; i++)
+            {
+
+                Console.Write(rez1.mass[i] + "; ");
+            }
+            Massiv mass3 = new Massiv(5);
+            Console.WriteLine("\nMassive3: ");
+            mass3.InputDataRandom();
+            for (int i = 0; i < mass3.size; i++)
+            {
+
+                Console.Write(mass3.mass[i] + "; ");
+            }
+            Console.Write("\nСкладываем первый и третий массив: ");
+            Massiv rez2 = mass1.Add(mass3);
+        }
+    }
+}
